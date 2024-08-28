e76e61bd1caadd045a2576a07518769efcfa8c9f
Mehmet <mehmetKmrc1109@gmail.com>
2024-08-28 10:03:49 +0300

    feat: return statements aded on middleware.go

--- server/middleware.go
+++ server/middleware.go
@@ -17,20 +17,20 @@ func authMiddleware(maker token.PasetoMaker) fiber.Handler{
 	return func(c fiber.Ctx) error {
 		authHeader := c.Get(authorizationHeaderKey)
 		if authHeader == "" {
-			c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error": "No Header was passed"})
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error": "No Header was passed"})
 		}
 		fields := strings.Fields(authHeader)
 		if len(fields) != 2 {
-			c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error":"Invalid or Missing Bearer Token"})
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error":"Invalid or Missing Bearer Token"})
 		}
 		authType := fields [0]
 		if strings.ToLower(authType) != authorizationHeaderBearerType{
-			c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error": "Authorization Type Not Supported"})
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error": "Authorization Type Not Supported"})
 		}
 		token := fields[1]
 		_, err := maker.VerifyToken(token)
 		if err != nil{
-			c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error": "Access Token Not Valid"})
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error": "Access Token Not Valid"})
 			
 		}
 		return c.Next()
